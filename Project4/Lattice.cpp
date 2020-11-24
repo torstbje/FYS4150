d73533ee24baf03cdbb10247bc83d6fd6e8f9e03
torstbje <torstbje@student.matnat.uio.no>
2020-11-24 17:58:05 +0100

    Resolve merge

--- Project4/Lattice.cpp
+++ Project4/Lattice.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 using namespace arma;
 Lattice::Lattice(int L,string initState,double temp){
-  srand(3);                             //seed
+  srand(2);                             //seed
   dim = L;
   energyProb = vec(pow(dim,2)+1);
   if (initState == "alligned"){
